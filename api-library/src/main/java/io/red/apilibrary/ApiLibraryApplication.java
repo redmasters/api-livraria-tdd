3a62f38ed96be7cb0fb98e2432a82542c92f797a
Erasmo Jr <erasmo.junior@gmail.com>
2022-05-07 23:06:31 -0300

    Projeto Api-Books criada

---
+++ api-library/src/main/java/io/red/apilibrary/ApiLibraryApplication.java
@@ -0,0 +1,13 @@
+package io.red.apilibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLibraryApplication.class, args);
+    }
+
+}
