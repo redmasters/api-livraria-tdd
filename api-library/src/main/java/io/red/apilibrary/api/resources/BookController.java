fa532a5152a0293642b7cf9f777c6ef2949a0677
Erasmo Jr <erasmo.junior@gmail.com>
2022-05-09 23:01:20 -0300

    feature: BookController, adicionado log ao criar Livro

--- api-library/src/main/java/io/red/apilibrary/api/resources/BookController.java
+++ api-library/src/main/java/io/red/apilibrary/api/resources/BookController.java
@@ -3,12 +3,15 @@ package io.red.apilibrary.api.resources;
 import io.red.apilibrary.api.request.BookRequest;
 import io.red.apilibrary.api.resources.response.BookResponse;
 import io.red.apilibrary.api.services.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/books")
 public class BookController {
+    private final static Logger log = LoggerFactory.getLogger(BookController.class.getSimpleName());
 
     BookService service;
 
@@ -20,6 +23,7 @@ public class BookController {
     @ResponseStatus(HttpStatus.CREATED)
     public BookResponse createBook(@RequestBody BookRequest request){
         BookResponse entity = service.save(request);
+        log.info("Book title {} and isnb nr.: {}, CREATED", entity.title(), entity.isbn());
         return new BookResponse(
                 entity.id(),
                 entity.title(),
